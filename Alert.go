1e623dda49160ab80764a24152946c1a663e3079
pmbibe <53257014+pmbibe@users.noreply.github.com>
2020-05-06 11:11:50 +0700

    Update Alert.go

--- Alert.go
+++ Alert.go
@@ -1,30 +1,34 @@
 package main
 import(
+	_"fmt"
 	"log"
 	"database/sql"
 	"os/exec"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func checkServiceRunning(service string, server string, lastStatusService uint) {
+func checkServiceRunning(service string, server string, lastStatusService uint, db *sql.DB) {
 	serviceName := "./exitCode.sh " + service + " " + server + " ;echo $?"
 	StatusCode := exec.Command("sh", "-c", serviceName)
 	statusCode, _ := StatusCode.Output()
 	sttCode := string(statusCode)
 	if sttCode == "0\n" && (lastStatusService != 0) {
-		log.Print("Service is running")
-		log.Print(changeStatustoOK(service,server))
+		log.Print("Service %s on %s is Running", service,server)
+		slect, _ :=db.Query(changeStatustoOK(service,server))
+		defer slect.Close()
 	} else if sttCode != "0\n" && (lastStatusService != 1) {
-		log.Print("Service is Dead")
-		log.Print(changeStatustoFail(service,server))
+		log.Print("Service %s on %s is Dead", service,server)
+		slect, _ :=db.Query(changeStatustoFail(service,server))
+		defer slect.Close()
 	}
 
 }
 
 func main(){
-	db, _ := sql.Open("mysql", "root:minhduc7b@tcp(192.168.141.204:3306)/monitor_byGo")
 
+db := connect()
 defer db.Close()
+for {
 
 slect, _ := db.Query("SELECT * FROM monitor")
 
@@ -37,10 +41,18 @@ type Tag struct {
 for slect.Next(){
 	var tag Tag
 	_ = slect.Scan(&tag.HOSTNAME,&tag.SERVICE,&tag.STATUSCODE)
-	checkServiceRunning(tag.SERVICE, tag.HOSTNAME, tag.STATUSCODE)
+	checkServiceRunning(tag.SERVICE, tag.HOSTNAME, tag.STATUSCODE, db)
 	
 }
 }
+
+
+}
+func connecttoDB(user,password,hostname,port,database string ) string {
+	return user +":"+password +"@" + "tcp(" + hostname + ":" +port +")/"+ database
+
+}
+
 func changeStatustoOK(service string, server string) string {
 	return "UPDATE monitor SET statusCode = 0 where Hostname = '" + server + "' AND Service = '" + service +"'"
 	
@@ -48,4 +60,8 @@ func changeStatustoOK(service string, server string) string {
 func changeStatustoFail(service string, server string) string {
 	return "UPDATE monitor SET statusCode = 1 where Hostname = '" + server + "' AND Service = '" + service +"'"
 	
+}
+func connect() *sql.DB {
+	db, _ := sql.Open("mysql", connecttoDB("root","minhduc7b","192.168.141.204","3306","monitor_byGo"))
+	return db
 }
